f7bb633ac7817e095f62b8cab2ddc4bfebbc4e94
kaiqiz8 <kzhang49@binghamton.edu>
2026-02-12 08:25:41 -0800

    add basic p2p tcp transport and main

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/kaiqiz/distributedfilesystem/p2p"
+)
 
 func main() {
+	// TCP server
+	tr := p2p.NewTCPTransport(":3000")
+	if err := tr.ListenAndAccpet(); err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("We Gucci!")
+	select {}
 }
--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -10,4 +10,8 @@ func TestTCPTransport(t *testing.T) {
 	listenAddr := ":4000"
 	tr := NewTCPTransport(listenAddr)
 	assert.Equal(t, tr.listenAddress, listenAddr)
+
+	assert.Nil(t, tr.ListenAndAccpet())
+
+	select {}
 }
